9aed68ed2e6800f8de89422488b3d2fb4e282395
Alexander Manukian <alikmanukian@gmail.com>
2024-01-18 20:10:00 +0400

    install and configure laravel/pint eslint prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+    "plugins": ["prettier"],
+    rules: {
+        "prettier/prettier": "error",
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "single"],
+        semi: ["error", "never"],
+        "vue/multi-word-component-names": "off",
+    },
+};
